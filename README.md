318b24db2ec9db550f099826ea1c7f36ba668137
Somnuk Wongkhan <somnuk.wk@gmail.com>
2016-06-06 01:30:39 +0700

    Add check list

--- README.md
+++ README.md
@@ -2,3 +2,23 @@
 
 - http://www.fluentassertions.com
 
+- [x] Basic assertsions
+- [x] Nullable types
+- [x] Boolean
+- [x] Strings
+- [x] Numeric
+- [x] Date and times
+- [ ] TimeSpans
+- [ ] Collections
+- [ ] Dictionaries
+- [ ] Guids
+- [ ] Enums
+- [ ] Exceptions
+- [ ] Object grpah comparision
+- [ ] Event monitoring
+- [ ] Type method and property assertions
+- [ ] Asembly reference
+- [ ] Xml classes
+- [ ] Exception time
+- [ ] Extensibility
+
--- TryFluentAssertions/FluentSpec.cs
+++ TryFluentAssertions/FluentSpec.cs
@@ -9,6 +9,58 @@ namespace TryFluentAssertions {
 		}
 
 		[Test]
+		public void BeAfter() {
+			DateTime.Now.Should().BeAfter(1.February(2000));
+			new DateTime(2222, 1, 1).Should().HaveDay(1).And.HaveMonth(1).And.HaveYear(2222);
+		}
+
+		[Test]
+		public void BeGreaterThan() {
+			(100).Should().BeGreaterThan(50);
+		}
+
+		[Test]
+		public void Match() {
+			"wk@gmail.com".Should().Match("*@*");
+		}
+
+		[Test]
+		public void NotNull() {
+			"".Should().NotBeNull();
+
+			string x = null;
+			x.Should().BeNull();
+		}
+
+		[Test]
+		public void BeFalse() {
+			var x = false;
+			x.Should().BeFalse();
+			(!x).Should().BeTrue();
+		}
+
+		[Test]
+		public void NotHaveValue() {
+			DateTime? date = null;
+			date.Should().NotHaveValue();
+
+			date = DateTime.Now;
+			date.Should().HaveValue();
+		}
+
+		[Test]
+		public void NotBe() {
+			var x = 100;
+			x.Should().NotBe(101);
+		}
+
+		[Test]
+		public void Be() {
+			var x = 100;
+			x.Should().Be(100);
+		}
+
+		[Test]
 		public void StartWith() {
 			var actual = "ABCDEFG";
 			actual.Should().StartWith("ABC");
